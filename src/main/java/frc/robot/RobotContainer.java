bc3d2d5ecba80bd57365f6c58cff7075934b9010
Acoliver102 <55566341+Acoliver102@users.noreply.github.com>
2021-03-11 15:41:21 -0600

    small fixes

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -2,14 +2,15 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
-import edu.wpi.first.wpilibj.command.Command;
 import frc.robot.commands.*;
 import frc.robot.subsystems.Chassis;
 
 public class RobotContainer {
 
     public static final Chassis mChassis = new Chassis();
-    public static XboxController mController = new XboxController(0);
+    public static final XboxController mController = new XboxController(0);
+
+
 
 
 
@@ -25,7 +26,7 @@ public class RobotContainer {
 
     private void configureButtonBindings() {
         new JoystickButton(mController, XboxController.Button.kBumperRight.value)
-                .whenPressed(new ResetGyro());
+                .whenPressed(new ResetGyro(mChassis));
 
     }
 
--- src/main/java/frc/robot/commands/ResetGyro.java
+++ src/main/java/frc/robot/commands/ResetGyro.java
@@ -1,5 +1,6 @@
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.RobotContainer;
@@ -7,13 +8,18 @@ import frc.robot.subsystems.Chassis;
 
 public class ResetGyro extends InstantCommand {
 
+    private final Chassis cChassis;
 
-
+    public ResetGyro(Chassis chassis) {
+        cChassis = chassis;
+    }
 
     @Override
     public void initialize() {
         System.out.println("Trying to Reset");
     }
+
+
 
 
 
